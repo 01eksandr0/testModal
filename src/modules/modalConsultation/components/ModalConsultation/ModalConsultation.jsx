9aaf5b6398dc1df3b7d9a54e37198a653d24e909
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-04-10 19:40:47 +0300

    1

--- src/modules/modalConsultation/components/ModalConsultation/ModalConsultation.jsx
+++ src/modules/modalConsultation/components/ModalConsultation/ModalConsultation.jsx
@@ -10,24 +10,26 @@ const ModalConsultation = () => {
   const closeModal = useCallback(() => setModal(), [setModal]);
 
   return (
-    <div className={s.modal}>
-      <div className={s.container}>
-        <div className={s.imgContainer}>
-          <MyPicture />
-          <button className={s.btnClose} onClick={closeModal}>
-            <svg className={s.icon} width={24} height={24}>
-              <use xlinkHref={`${sprite}#icon-cross`}></use>
-            </svg>
-          </button>
-        </div>
-        <div className={s.formContainer}>
-          <h2 className={s.title}>Записатися на консультацію</h2>
-          <button className={s.btnSecond} onClick={() => closeModal()}>
-            <svg className={s.icon} width={24} height={24}>
-              <use xlinkHref={`${sprite}#icon-cross`}></use>
-            </svg>
-          </button>
-          <FormConsultation />
+    <div style={{ paddingBottom: 60 }}>
+      <div className={s.modal}>
+        <div className={s.container}>
+          <div className={s.imgContainer}>
+            <MyPicture />
+            <button className={s.btnClose} onClick={closeModal}>
+              <svg className={s.icon} width={24} height={24}>
+                <use xlinkHref={`${sprite}#icon-cross`}></use>
+              </svg>
+            </button>
+          </div>
+          <div className={s.formContainer}>
+            <h2 className={s.title}>Записатися на консультацію</h2>
+            <button className={s.btnSecond} onClick={() => closeModal()}>
+              <svg className={s.icon} width={24} height={24}>
+                <use xlinkHref={`${sprite}#icon-cross`}></use>
+              </svg>
+            </button>
+            <FormConsultation />
+          </div>
         </div>
       </div>
     </div>
