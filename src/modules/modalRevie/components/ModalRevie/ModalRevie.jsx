0a071faf6061c9756b672d378f58584dded92ac4
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-04-11 18:53:53 +0300

    Update ModalRevie.jsx

--- src/modules/modalRevie/components/ModalRevie/ModalRevie.jsx
+++ src/modules/modalRevie/components/ModalRevie/ModalRevie.jsx
@@ -9,7 +9,7 @@ const ModalRevie = () => {
   const setModal = useModal();
   const closeModal = useCallback(() => setModal(), [setModal]);
   return (
-    <div style={{ paddingBottom: 40 }}>
+    <div style={{ paddingBottom: 60 }}>
       <div className={s.modal}>
         <div className={s.container}>
           <button className={s.btnClose} onClick={closeModal}>
