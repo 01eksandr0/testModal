212f375ecceb1782c4b8f0b65334a4ae26e6ae02
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-04-11 08:50:57 +0300

    1

--- src/modules/modalRevie/components/ModalRevie/ModalRevie.jsx
+++ src/modules/modalRevie/components/ModalRevie/ModalRevie.jsx
@@ -9,7 +9,7 @@ const ModalRevie = () => {
   const setModal = useModal();
   const closeModal = useCallback(() => setModal(), [setModal]);
   return (
-    <div style={{ paddingBottom: 60 }}>
+    <div>
       <div className={s.modal}>
         <div className={s.container}>
           <div className={s.imgContainer}>
